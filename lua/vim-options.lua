2a74482f37ebdcad2de4718c0ec9b17c57a675b3
charliecharlie <jeancharles552@gmail.com>
2025-06-01 07:34:39 -0300

    feat(vim): add new vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.relativenumber = true
+vim.opt.cursorcolumn = true
+vim.opt.wrap = true
+vim.opt.termguicolors = true
+vim.o.statuscolumn = "%s %l %r"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Exit" })
