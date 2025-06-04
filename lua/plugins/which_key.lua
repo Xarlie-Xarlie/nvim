a8550e5a35efa0b943f8a00ccf6d67df4a772fb7
charliecharlie <jeancharles552@gmail.com>
2025-06-04 20:28:15 -0300

    feat(which-key): add plugin management keybindings
    Add a Plugins group and keybindings under <leader>p for managing plugins
    with Lazy.nvim, including install, sync, status, clean, update, profile,
    log, and debug commands. Also customize which-key window appearance with
    a double border, centered title, and padding for improved UI clarity.

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -4,6 +4,17 @@ return {
   dependencies = {
     { 'echasnovski/mini.nvim', version = '*' },
   },
+  opts = {
+    win = {
+      border = "double",        -- none, single, double, shadow
+      title = "Keybindings",
+      title_pos = "center",     -- title position: left, center, right
+      padding = { 2, 4, 2, 4 }, -- [top, right, bottom, left]
+      wo = {
+        winblend = 0,
+      }
+    },
+  },
   init = function()
     require("which-key").add({
       -- General
@@ -23,6 +34,16 @@ return {
       { "<leader>sC", "<cmd>Telescope commands<cr>",                                      desc = "Commands", },
       { "<leader>sT", "<cmd>lua CustomLiveGrep()<cr>",                                    desc = "Symbols", },
       { "<leader>sb", "<cmd>Telescope buffers<cr>",                                       desc = "Buffers", },
+      -- Plugins
+      { "<leader>p",  group = "Plugins" },
+      { "<leader>pi", "<cmd>Lazy install<cr>",                                            desc = "Install" },
+      { "<leader>ps", "<cmd>Lazy sync<cr>",                                               desc = "Sync" },
+      { "<leader>pS", "<cmd>Lazy clear<cr>",                                              desc = "Status" },
+      { "<leader>pc", "<cmd>Lazy clean<cr>",                                              desc = "Clean" },
+      { "<leader>pu", "<cmd>Lazy update<cr>",                                             desc = "Update" },
+      { "<leader>pp", "<cmd>Lazy profile<cr>",                                            desc = "Profile" },
+      { "<leader>pl", "<cmd>Lazy log<cr>",                                                desc = "Log" },
+      { "<leader>pd", "<cmd>Lazy debug<cr>",                                              desc = "Debug" },
     })
   end
 }
