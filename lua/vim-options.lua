2e3aaf9b8237ee75b7cdfb1fcf4326f5bc7a4789
Charlie Charlie <jean.charles@isaac.com.br>
2025-06-09 11:34:09 -0300

    feat(ui): improve line number and status column display
    Switch to using window-scoped options for line numbers and relative
    numbers.
    Update the statuscolumn to show the current line and relative line
    numbers with a custom format.
    This enhances the usability and visual clarity of the editor interface.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,11 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.opt.relativenumber = true
 vim.opt.cursorcolumn = true
 vim.opt.wrap = true
 vim.opt.termguicolors = true
-vim.o.statuscolumn = "%s %l %r"
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.statuscolumn = [[%!v:lnum . ' ' . (v:lnum == line('.') ? 0 : abs(v:lnum - line('.')))]]
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.splitright = true
